f2c2f1dc9adbbf015e72892b72f63a0bcbed3aef
huangyuxin2023 <huangyuxin@isrc.iscas.ac.cn>
2023-10-24 10:15:22 +0800

    修改hitrace

--- include/hitrace_meter/hitrace_meter.h
+++ include/hitrace_meter/hitrace_meter.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,28 +13,7 @@
  * limitations under the License.
  */
 
-#ifndef MOCK_NATIVE_INCLUDE_HILOG_HITRACE_METER_H
-#define MOCK_NATIVE_INCLUDE_HILOG_HITRACE_METER_H
-
-#include <string>
-
-namespace OHOS {
-#ifdef __cplusplus
-extern "C" {
-#endif
 #define HITRACE_TAG_ZIMAGE (-1)
-/**
- * Track the beginning of a context.
- */
-void StartTrace(uint64_t label, const std::string& value, float limit = -1);
 
-/**
- * Track the end of a context.
- */
-void FinishTrace(uint64_t label);
-
-#ifdef __cplusplus
-}
-#endif
-#endif // MOCK_NATIVE_INCLUDE_HILOG_HITRACE_METER_H
-}
+#define StartTrace(...)
+#define FinishTrace(...)
